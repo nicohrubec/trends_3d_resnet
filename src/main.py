a666c7effe7047ff505ab39001c7644f2e4d2fb1
Nico Hrubec <nicolas.hrubec@outlook.com>
2020-06-17 16:45:34 +0200

    Set seed.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
-from src import preprocess
+from src import preprocess, utils
 
+# hyperparameters
 compression = True
+SEED = 42
 
 if __name__ == '__main__':
     if compression:
         preprocess.compress_images(mode='train')
         preprocess.compress_images(mode='test')
+
+    utils.set_seed(SEED)
+    
---
+++ src/utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+import torch
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
